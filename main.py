9db23a489c39955848aeaf0bdc7e77422c74ae50
Rbuerk9522 <110101078+Rbuerk9522@users.noreply.github.com>
2025-10-01 16:22:42 +0000

    3rd step complete. Moving to step 4

--- main.py
+++ main.py
@@ -65,10 +65,17 @@ def go(config: DictConfig):
     )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+           mlflow.run(
+        os.path.join(hydra.utils.get_original_cwd(), "src", "data_check") ,
+        entry_point="main",
+        parameters={
+            "csv": config["data_check"]["csv"],
+            "ref": config["data_check"]["ref"],
+            "kl_threshold": config["data_check"]["kl_threshold"],
+            "min_price": config["data_check"]["min_price"],
+            "max_price": config["data_check"]["max_price"]
+        }
+    )
 
         if "data_split" in active_steps:
             ##################
